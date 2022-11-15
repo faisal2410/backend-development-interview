a6d9eefa3e9027b47ae52b3e653f1b93c9b6491a
Faisal ahmed <faisal2410@yahoo.com>
2022-11-15 14:08:42 +0600

    initial commit

---
+++ 2022-11-02T19-47-42.325Z-assignment_5/assignment_5/interviewQuestion.js
@@ -0,0 +1,50 @@
+// 1.  What is the difference between Java & JavaScript?
+// 2. What is JavaScript?
+// 3.  What are the data types supported by JavaScript?
+// 4. Is JavaScript a case-sensitive language?
+// 5. How can you create an object in JavaScript?
+// 6. How can you create an Array in JavaScript?
+// 7. What is a name function in JavaScript & how to define it?
+// 8. What is argument objects in JavaScript?
+// 9. What are the scopes of a variable in JavaScript?
+// 10. What is Callback function and how it works?
+// 11. How to read a cookie using JavaScript?
+// 12. How to delete a cookie using JavaScript?
+// 13. What is the difference between Attributes and Property?
+// 14. What is the difference between null & undefined?
+// 15. What is the difference between the operators ‘==‘ & ‘===‘?
+// 16. What is the difference between undeclared & undefined?
+// 17. What is an event bubbling in JavaScript?
+// 18. What is NaN in JavaScript?
+// 19. How do JavaScript primitive/object types passed in functions?
+// 20. How can you convert the string of any base to integer in JavaScript? 
+// 21. What are Exports & Imports?
+// 22.  What is the ‘Strict’ mode in JavaScript?
+// 23. What is the difference between Call & Apply?
+// 24. How to empty an Array in JavaScript?
+// 25. Why do we use the word “debugger” in javascript?
+// 26. Is javascript a statically typed or a dynamically typed language?
+// 27. What is an Immediately Invoked Function in JavaScript?
+// 28. What is currying in JavaScript?
+// 29. Explain the difference between frontend and backend development?
+// 30. What is NPM?
+// 31. What are the modules in Node.js?
+// 32. Which database is more popularly used with Node.js?
+// 33. What are some of the most commonly used libraries in Node.js?
+// 34. What is API?
+// 35. Why MongoDB is the best NoSQL database?
+// 36. What is the importance of profiler in MongoDB?
+// 37. What is the purpose of the save() method?
+// 38. What is the purpose of the DB command?
+// 39. How do we remove a document from the collection?
+// 40. What does ObjectId contain on MongoDB?
+// 41. What are the CRUD operations of MongoDB?
+// 42. What are the datatypes of MongoDB?
+// 43. What are the different kinds of Indexes in MongoDB?
+// 44. How does MongoDB store the data?
+// 45. How do we retrieve MongoDB databases in Javascript Array?
+// 46. Does MongoDB support primary-key, foreign-key relationship?
+// 47. Does MongoDB need a lot of RAM?
+// 48. Explain the structure of ObjectID in MongoDB.
+// 49. What is the syntax to create a collection
+// 50. What are alternatives to MongoDB?
